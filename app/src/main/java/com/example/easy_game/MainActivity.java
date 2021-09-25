6163a905f6bfb4a2d81ea24ec485d462daf9a841
ExTeZil <vasiagakman.v.g.40@gmail.com>
2021-09-25 20:12:33 +0300

    commit

--- app/src/main/java/com/example/easy_game/MainActivity.java
+++ app/src/main/java/com/example/easy_game/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.easy_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
     }
 }
